b65e3d51017da640bf73aa957b04496ac7e665aa
Jakob Nunnendorf <Jnunnendorf@icloud.com>
2025-10-17 19:02:14 +0200

    add empty Board component to main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
-import Image from 'next/image'
+import Board from "@/components/Board";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <main>
-      
+      <Board />
     </main>
-  )
+  );
 }
