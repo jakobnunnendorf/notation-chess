5ef52b0612c6cbfd78bfd127f3a71ca390d2703b
Jakob Nunnendorf <Jnunnendorf@icloud.com>
2025-10-18 19:46:31 +0200

    show attack tiles red

--- components/Tile.tsx
+++ components/Tile.tsx
@@ -8,16 +8,32 @@ export default function Tile({
   even: boolean;
   coordinate: [number, number];
 }) {
-  const { setActiveTile, setActivePiece, availableTiles, activeTile } =
-    useGame();
+  const {
+    setActiveTile,
+    setActivePiece,
+    availableTiles,
+    activeTile,
+    occupiedSquares,
+    activePiece,
+  } = useGame();
   const [available, setAvailable] = useState<boolean>(false);
+  const [occupiedByEnemy, setOccupiedByEnemy] = useState(false);
 
   useEffect(() => {
     const available = availableTiles.some(
       (tile) => tile[0] === coordinate[0] && tile[1] === coordinate[1]
     );
     setAvailable(available);
-  }, [availableTiles]);
+
+    const occupier = occupiedSquares.find(
+      (square) =>
+        square.coord[0] === coordinate[0] && square.coord[1] === coordinate[1]
+    );
+    if (!occupier || !available) return;
+    const occupierIsEnemy =
+      occupier && activePiece && occupier?.colour !== activePiece?.colour;
+    setOccupiedByEnemy(Boolean(occupier && occupierIsEnemy));
+  }, [availableTiles, occupiedSquares, activePiece, coordinate]);
 
   return (
     <button
@@ -30,7 +46,13 @@ export default function Tile({
         }
       }}
       className={`border ${
-        available ? "bg-green-200" : even ? "bg-gray-200" : "bg-white"
+        occupiedByEnemy
+          ? "bg-red-200"
+          : available
+          ? "bg-green-200"
+          : even
+          ? "bg-gray-200"
+          : "bg-white"
       } h-12 w-12`}
     ></button>
   );
