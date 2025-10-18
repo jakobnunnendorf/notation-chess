8d38eaa6b45d546f043ba96e151854cf58b5ca5d
Jakob Nunnendorf <Jnunnendorf@icloud.com>
2025-10-18 23:41:22 +0200

    Update piece meta data completely

---
+++ components/ProgressBar.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import { useGame } from "@/context/GameContext";
+import React, { useEffect, useState } from "react";
+
+function BlackProgress({ progress }: { progress: number }) {
+  return (
+    <div style={{ height: progress * 384 }} className="w-full bg-black"></div>
+  );
+}
+
+export default function ProgressBar() {
+  const { piecesMetaData } = useGame();
+  const [progress, setProgress] = useState(0.5);
+
+  useEffect(() => {
+    const blackCount = piecesMetaData.filter(
+      (piece) => piece.colour === "black"
+    ).length;
+    const whiteCount = piecesMetaData.filter(
+      (piece) => piece.colour === "white"
+    ).length;
+    const total = whiteCount + blackCount || 1; // Avoid div/0
+    const progress = whiteCount / total;
+
+    setProgress(progress);
+  }, [piecesMetaData]);
+  return (
+    <div className="flex items-end w-8 border h-96">
+      <BlackProgress progress={progress} />
+    </div>
+  );
+}
