4f104ebb955394654534cf7690a494b76b7d4c62
Jakob Nunnendorf <Jnunnendorf@icloud.com>
2025-10-19 15:15:22 +0200

    flip annotation with board

--- components/Tile.tsx
+++ components/Tile.tsx
@@ -21,6 +21,7 @@ export default function Tile({
     setPiecesMetaData,
     activePiece,
     setAvailableTiles,
+    boardSide,
   } = useGame();
   const [available, setAvailable] = useState<boolean>(false);
   const [occupiedByEnemy, setOccupiedByEnemy] = useState(false);
@@ -51,7 +52,7 @@ export default function Tile({
         setAvailableTiles([]);
         setActivePiece(null);
       }}
-      className={`relative border ${
+      className={`relative ${
         occupiedByEnemy
           ? "bg-red-200"
           : available
@@ -62,15 +63,27 @@ export default function Tile({
       } h-12 w-12`}
     >
       {/* Left side: show rank numbers (8..1) only on the first column */}
-      {coordinate[0] === 1 ? (
-        <span className="absolute top-0 left-0 px-1 text-xs select-none">
+      {coordinate[0] === 8 && boardSide === "black" ? (
+        <span className="absolute top-0 right-0 px-1 text-[0.5rem] opacity-50 select-none">
+          {coordinate[1]}
+        </span>
+      ) : coordinate[0] === 1 && boardSide === "white" ? (
+        <span className="absolute bottom-0 left-0 px-1 text-[0.5rem] opacity-50 rotate-180 select-none">
           {coordinate[1]}
         </span>
       ) : null}
 
       {/* Bottom side: show file letters (a..h) only on the last row */}
-      {coordinate[1] === 8 ? (
-        <span className="absolute bottom-0 right-0 px-1 text-xs select-none">
+      {coordinate[1] === 8 && boardSide === "black" ? (
+        <span className="absolute bottom-0 left-0 px-1 text-[0.5rem] opacity-50 select-none">
+          {
+            ["a", "b", "c", "d", "e", "f", "g", "h"].reverse()[
+              coordinate[0] - 1
+            ]
+          }
+        </span>
+      ) : coordinate[1] === 1 && boardSide === "white" ? (
+        <span className="absolute top-0 right-0 px-1 text-[0.5rem] opacity-50 rotate-180 select-none">
           {
             ["a", "b", "c", "d", "e", "f", "g", "h"].reverse()[
               coordinate[0] - 1
